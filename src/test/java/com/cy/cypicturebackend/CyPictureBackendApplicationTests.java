03edead56e6c3319edbf66f678fe45cd52878238
cy <chenyang178@163.com>
2025-07-17 20:02:19 +0800

    云图库项目初始化

---
+++ src/test/java/com/cy/cypicturebackend/CyPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy.cypicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
