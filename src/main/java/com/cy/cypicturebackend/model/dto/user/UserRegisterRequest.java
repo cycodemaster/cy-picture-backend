7317ab671c8225178f8ae14f0b9819b2f903c962
cy <chenyang178@163.com>
2025-08-11 09:25:14 +0800

    用户注册请求类序列化

--- src/main/java/com/cy/cypicturebackend/model/dto/user/UserRegisterRequest.java
+++ src/main/java/com/cy/cypicturebackend/model/dto/user/UserRegisterRequest.java
@@ -5,10 +5,12 @@ import lombok.Data;
 import java.io.Serializable;
 
 /**
- * 用户注册请求
+ * 用户注册请求类
  */
 @Data
 public class UserRegisterRequest implements Serializable {
+
+    private static final long serialVersionUID = -9065814710237889540L;
 
     /**
      * 账号
