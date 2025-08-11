26f6b95a3accf89089babe99ea6a37103c394dbd
cy <chenyang178@163.com>
2025-08-11 13:22:29 +0800

    脱敏用户vo添加注解

--- src/main/java/com/cy/cypicturebackend/model/vo/LoginUserVO.java
+++ src/main/java/com/cy/cypicturebackend/model/vo/LoginUserVO.java
@@ -1,12 +1,11 @@
 package com.cy.cypicturebackend.model.vo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
 public class LoginUserVO implements Serializable {
 
     private static final long serialVersionUID = -912140099054157032L;
